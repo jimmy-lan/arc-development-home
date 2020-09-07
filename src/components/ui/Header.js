50e847ca309cdf82acf379ca431709a2fa849d77
Jimmy <jimmylanbcn@gmail.com>
2020-09-06 20:47:14 -0400

    Update header logo

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -1,18 +1,35 @@
 import React from "react";
-import { AppBar, Toolbar, Typography } from "@material-ui/core";
+import { AppBar, Toolbar } from "@material-ui/core";
+import { makeStyles } from "@material-ui/styles";
 
 import { ElevationScroll } from "../common";
+import logo from "../../assets/images/logo.png";
+
+const useStyles = makeStyles((theme) => ({
+  toolbarMargin: {
+    ...theme.mixins.toolbar,
+  },
+  logo: {
+    height: "7em",
+  },
+}));
 
 export default function Header(props) {
+  const classes = useStyles();
+
   return (
-    <ElevationScroll>
-      <AppBar position="fixed">
-        {/* The reason to include the Toolbar component is to make sure that
+    <>
+      <ElevationScroll>
+        <AppBar position="fixed">
+          {/* The reason to include the Toolbar component is to make sure that
       things span horizontally, not stack up vertically. */}
-        <Toolbar>
-          <Typography>Arc Development</Typography>
-        </Toolbar>
-      </AppBar>
-    </ElevationScroll>
+          <Toolbar disableGutters>
+            <img alt="Poly Team Logo" className={classes.logo} src={logo} />
+          </Toolbar>
+        </AppBar>
+      </ElevationScroll>
+      {/* Push contents down to proper region so they are not covered by the header */}
+      <div className={classes.toolbarMargin} />
+    </>
   );
 }
